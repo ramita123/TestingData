86a45a008afa7baab4ddb3729cd628e408e29509
ramita123 <ramitasambyal@gmail.com>
2020-03-09 18:03:07 +0530

    thrd commintr

---
+++ mvn/src/test/java/emailMvn/mvn/demo.java
@@ -0,0 +1,17 @@
+package emailMvn.mvn;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	@Test
+	public void demo()
+	{
+		System.out.println("babbyyy");
+	}
+	@Test
+	public void demo2()
+	{
+		System.out.println("bbaayyyyyyyyyyyy");
+	}
+
+}
